67e3430fc2a7f26b13ce61f8f832ad6bc32dab17
viktoriababat <viktoria.babatova@yandex.ru>
2023-06-02 14:30:36 +0500

    Add post test success again

--- src/test/java/ru/netology/rest/MyPostmanTest.java
+++ src/test/java/ru/netology/rest/MyPostmanTest.java
@@ -20,7 +20,7 @@ class MyPostmanTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some value"))
         ;
     }
 }
